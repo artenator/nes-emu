748a74574e562210d4f5b4ffe041c981548e157e
Arte Ebrahimi <arteebrahimi@gmail.com>
2018-12-01 17:31:00 -0800

    fix conflict with apu

--- hardware/nes.go
+++ hardware/nes.go
@@ -5,15 +5,18 @@ import "image"
 type NES struct {
 	CPU *Cpu
 	PPU *Ppu
+	APU *Apu
 }
 
 func NewNES() NES {
 	newNes := NES{}
 	newNes.CPU = &Cpu{}
 	newNes.PPU = &Ppu{}
+	newNes.APU = &Apu{}
 	newNes.PPU.Frame = image.NewRGBA(image.Rect(0, 0, 256, 240))
 	newNes.CPU.nes = &newNes
 	newNes.PPU.nes = &newNes
+	newNes.APU.nes = &newNes
 	newNes.PPU.ppuAddrCounter = 0
 
 	return newNes
